23370a7320d3fae8caacd4d39969eefdcf1b2d9f
Kimberley-saka <kimberleysaka@gmail.com>
2023-07-28 12:52:51 +0000

    solution to task 2

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Acaching system that inherits from BaseCashing
+"""
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """
+    Is a caching system
+    """
+
+    def __init__(self):
+        """init method
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        add data to cache and discard
+        """
+        if key is None or item is None:
+            pass
+        else:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS \
+                    and key not in self.cache_data.keys():
+                first_key = next(iter(self.cache_data.keys()))
+                last_key, last_value = self.cache_data.popitem()
+                print("DISCARD: {}". format(last_key))
+
+            self.cache_data.update({key: item})
+
+    def get(self, key):
+        """
+        Returns the value of cache_data linked to key
+        """
+        if key is None or key not in self.cache_data.keys():
+            return None
+        return self.cache_data.get(key)
