34a78f24001469ba4a1039a131ffc2774d04b7a9
Kimberley saka <kimberleysaka@gmail.com>
2023-08-23 11:16:46 +0200

    solution to task 0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,29 @@
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    A caching system
+    """
+    def __init__(self):
+        """
+        initialise
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        add data to cache
+        """
+        if key is None or item is None:
+            pass
+        else:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Return the value in self.cache_data linked to key
+        """
+        if key is None or key not in self.cache_data.keys():
+            return None
+        return self.cache_data.get(key)
