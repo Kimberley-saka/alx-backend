0dadc34b38544d71c1c99c2ea8af993752c0fd86
Kimberley saka <kimberleysaka@gmail.com>
2023-08-30 15:58:00 +0200

    solution to task 7

---
+++ 0x02-i18n/7-app.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+"""
+simple flask app
+"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+from pytz import timezone, exceptions
+
+
+app = Flask(__name__)
+babel = Babel(app)
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+class Config(object):
+    """
+    language config
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def hello():
+    """
+    displays hello world
+    """
+    return render_template('6-index.html')
+
+
+def get_user():
+    """
+    get user
+    """
+    id = request.args.get('login_as')
+    if id:
+        return users.get(int(id))
+    return None
+
+
+@app.before_request
+def before_reqeust():
+    """
+    runs before other functions
+    """
+    user = get_user()
+    g.user = user
+
+
+@babel.localeselector
+def get_locale():
+    """
+    get best match locale
+    """
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+
+    if g.user:
+        locale = g.user.get('locale')
+        if locale and locale in app.config['LANGUAGES']:
+            return locale
+
+    locale = request.headers.get('locale', None)
+    if locale in app.config['LANGUAGES']:
+        return locale
+
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@babel.timezoneselector
+def get_timezone():
+    """
+    get timezone
+    """
+    tmzone = request.args.get('timezone', None)
+    if tmzone:
+        try:
+            return timezone(tmzone).zone
+        except exceptions.UnknownTimeZoneError:
+            pass
+
+    if g.user:
+        try:
+            tmzone = g.user.get('timezone')
+            return timezone(tmzone).zone
+        except exceptions.UnknownTimeZoneError:
+            pass
+
+    default_tz = app.config['BABEL_DEFAULT_TIMEZONE']
+    return default_tz
+
+
+if __name__ == '__main__':
+    app.run(port="5000", host="0.0.0.0", debug=True)
